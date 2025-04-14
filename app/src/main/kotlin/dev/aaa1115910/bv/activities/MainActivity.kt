860835c03c35fab98bd3f2b7d888a3fe406f4d23
LazzyFatty <i@haom.ren>
2025-04-15 01:56:50 +0800

    Update MainActivity.kt

--- app/src/main/kotlin/dev/aaa1115910/bv/activities/MainActivity.kt
+++ app/src/main/kotlin/dev/aaa1115910/bv/activities/MainActivity.kt
@@ -53,7 +53,7 @@ class MainActivity : ComponentActivity() {
 
             LaunchedEffect(Unit) {
                 scope.launch(Dispatchers.Default) {
-                    isMainlandChina = NetworkUtil.isMainlandChina()
+                    isMainlandChina = 0
                     isCheckingNetwork = false
                     keepSplashScreen = false
                 }
